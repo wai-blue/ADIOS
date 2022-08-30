1d46c2935ab68f745549e6f01954cb833588bef7
Peter <peter.alaxin@wai.sk>
2022-08-30 20:23:23 +0200

    HTML action

--- src/Core/UI.php
+++ src/Core/UI.php
@@ -2,12 +2,15 @@
 
 namespace ADIOS\Core;
 
-class UI {
-  public function __construct(&$adios, $params) {
+class UI
+{
+  public function __construct(&$adios, $params)
+  {
     $this->adios = $adios;
   }
 
-  public function create($component_name, $params = null) {
+  public function create($component_name, $params = null)
+  {
     list($component_class, $uid) = explode('#', $component_name);
 
     $params['component_class'] = $component_class;
@@ -19,52 +22,68 @@ class UI {
     return new $class_name($this->adios, $params);
   }
 
-  public function render($component_name, $params = null) {
+  public function render($component_name, $params = null)
+  {
     return $this->create($component_name, $params)->render();
   }
 
-  public function Title($params) {
+  public function Title($params)
+  {
     return $this->create('Title', $params);
   }
 
-  public function Form($params) {
+  public function Form($params)
+  {
     return $this->create('Form', $params);
   }
 
-  public function Input($params) {
+  public function Input($params)
+  {
     return $this->create('Input', $params);
   }
 
-  public function Tabs($params) {
+  public function Tabs($params)
+  {
     return $this->create('Tabs', $params);
   }
 
-  public function Table($params) {
+  public function Table($params)
+  {
     return $this->create('Table', $params);
   }
 
-  public function Tree($params) {
+  public function Html($params)
+  {
+    return $this->create('Html', $params);
+  }
+
+  public function Tree($params)
+  {
     return $this->create('Tree', $params);
   }
 
-  public function FileBrowser($params) {
+  public function FileBrowser($params)
+  {
     return $this->create('FileBrowser', $params);
   }
 
-  public function SettingsPanel($params) {
+  public function SettingsPanel($params)
+  {
     return $this->create('SettingsPanel', $params);
   }
 
-  public function Cards($params) {
+  public function Cards($params)
+  {
     return $this->create('Cards', $params);
   }
 
-  public function Window($params) {
+  public function Window($params)
+  {
     return $this->create('Window', $params);
   }
 
-  public function Button($params = []) {
+  public function Button($params = [])
+  {
     return $this->create('Button', $params);
   }
-
 }
