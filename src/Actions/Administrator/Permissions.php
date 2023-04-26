572ff5fb06fc4dbeef160123788092e7fe2e4181
Dusan <dusan.daniska@wai.sk>
2023-04-26 11:47:20 +0200

    parsovanie Actions adresarov

--- src/Actions/Administrator/Permissions.php
+++ src/Actions/Administrator/Permissions.php
@@ -22,12 +22,30 @@ class Permissions extends \ADIOS\Core\Action {
       $userRoles = $userRoleModel->getAll();
     }
 
+    // permissions podla routingu (najma pre modely)
     $permissions = [];
     foreach ($this->adios->routing as $routeParams) {
       if (!empty($routeParams['permission'])) {
         $permissions[] = $routeParams['permission'];
       }
     }
+
+    // permissions podla Actions adresarov
+    foreach ($this->adios->widgets as $widget) {
+
+      // TODO: recursive scandir
+      $widgetActions = @scandir("{$widget->myRootFolder}/Actions");
+
+      if (is_array($widgetActions)) {
+        foreach ($widgetActions as $action) {
+          if (substr($action, -4) == ".php") {
+            $permissions[] = "Widgets/{$widget->fullName}/Actions/".substr($action, 0, -4);
+          }
+        }
+      }
+    }
+
+    // sort
     sort($permissions);
 
     return [
