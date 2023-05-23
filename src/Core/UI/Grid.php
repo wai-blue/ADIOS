6df0580e6643675b23acc258c81f4d76dafed0f9
Holes70 <holespato@gmail.com>
2023-05-23 08:49:35 +0200

    grid

--- src/Core/UI/Grid.php
+++ src/Core/UI/Grid.php
@@ -17,11 +17,25 @@ namespace ADIOS\Core\UI;
  */
 class Grid extends \ADIOS\Core\UI\View {
 
+  public string $twigTemplate = "Core/UI/Grid";
+
   /**
    * @internal
    */
-  public function __construct($adios, $params = null) {
-    parent::__construct($adios, $params);
+  public function __construct($adios, ?array $params = null) {
+    $this->adios = &$adios;
+
+    $this->params = parent::params_merge([
+      "layout" => [],
+      "layoutSm" => [],
+      "layoutMd" => [],
+      "layoutLg" => [],
+      "areas" => []
+    ], $params);
+  }
+
+  public function getTwigParams(): array {
+    return $this->params;
   }
 
 }
