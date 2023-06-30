5c5ab95ab635ad625a4e3720b9fac06339358605
Michal Barnas <michal.blava@gmail.com>
2023-06-30 12:23:01 +0200

    updated according to review

--- src/Core/Views/Chart.php
+++ src/Core/Views/Chart.php
@@ -3,18 +3,23 @@
 
 namespace ADIOS\Core\Views;
 
-class Chart extends \ADIOS\Core\View {
+use ADIOS\Core\Loader;
+use ADIOS\Core\Model;
+use ADIOS\Core\View;
+
+class Chart extends View
+{
 
   public string $twigTemplate = "Core/UI/Chart";
-  private ?\ADIOS\Core\Model $model = null;
+  private ?Model $model = null;
 
-  public function __construct(?\ADIOS\Core\Loader $adios, array $params = []) {
+  public function __construct(?Loader $adios, array $params = [])
+  {
     $this->adios = $adios;
 
     $this->params = parent::params_merge([
       'type' => null, # pie, bar, line, radar, bubble, doughnut, polarArea, radar, scatter
-      'datasets' => null,
-      'function' => null,
+      'datasets' => null, # { data/model: .., dataColumns: .., labelColumn/labels: .., limit: .., function: ..}
     ], $params);
 
     if (empty($this->params['datasets'])) {
@@ -26,107 +31,108 @@ class Chart extends \ADIOS\Core\View {
         exit("UI/Chart: Data not provided. Please specify model or data properties in datasets.");
       }
 
-      if ($dataset['dataColumns'] == '') {
-        exit("UI/Chart: Data not provided. Please specify dataColumns property in datasets.");
-      }
-
       if ($dataset['labelColumn'] == '' && $dataset['labels'] == '') {
         exit("UI/Chart: Labels not provided. Please specify labelColumn or labels properties in datasets.");
       }
 
-      $this->params['labelModel'] = $this->adios
-        ->getModel($dataset['model'])
-      ;
+      if ($dataset['model'] != '') {
 
-      if ($dataset['where'] != '') {
-        $this->params['labelModel'] = $this->params['labelModel']
-          ->where(...array_merge_recursive($dataset['where']))
-        ;
-      }
+        if ($dataset['dataColumns'] == '') {
+          exit("UI/Chart: Data not provided. Please specify dataColumns property in datasets.");
+        }
 
-      $this->params['labelModel'] = array_unique(
-        array_merge_recursive(...
-          $this
-            ->params['labelModel']
-            ->select($dataset['labelColumn']['column'])
-            ->get()
-            ->toArray())[$dataset['labelColumn']['column']] ?? [])
-          ;
+        # Determines the amount of chart data rows, that need to be fetched
+        $limit = match ($dataset['function']) {
+          'count' => $dataset['limit'] ?? 10,
+          default => -1,
+        };
 
-      # Determines the limit of the chart data, that needs to be fetched
-      $limit = match ($dataset['function']) {
-        'count' => $dataset['limit'] ?? 10,
-        default => count($this->params['labelModel']),
-      };
-
-      # Data processing
-      foreach ($dataset['dataColumns'] as $col_def) {
-        $col = key($col_def);
-
-        $dataset['data'] = $this->adios->getModel($dataset['model']);
+        $labelQuery = $this->adios
+          ->getModel($dataset['model']);
 
         if ($dataset['where'] != '') {
-          $dataset['data'] = $dataset['data']->where(...array_merge_recursive($dataset['where']));
+          $labelQuery = $labelQuery
+            ->where(...array_merge_recursive($dataset['where']));
         }
 
-        switch ($dataset['function']) {
-          # Counts all found rows, useful if there are more rows than labels
-          case 'count':
-            $data = [];
+        $labelQuery = $labelQuery
+          ->select($dataset['labelColumn']['column'])
+          ->get();
 
-            foreach($this->params['labelModel'] as $label_value) {
-              $data[] = $dataset['data']
-                ->where($dataset['labelColumn']['column'], "=", $label_value)
-                ->limit($limit)
-                ->count()
-              ;
-            }
+        $this->params['labelModel'] = array_unique(
+          array_merge_recursive(
+            ...$labelQuery->toArray()
+          )[$dataset['labelColumn']['column']] ?? []);
 
-            $dataset['data'] = $data;
-          break;
+        # Fills in the value of the limit, if it could not be determined before
+        if ($limit = -1) {
+          $limit = count($this->params['labelModel']);
+        }
 
-          # Sums all found rows, useful if there are more rows than labels
-          case 'sum':
-            $data = [];
-            foreach($this->params['labelModel'] as $label_value) {
-              $data[] = $dataset['data']
-                ->where($dataset['labelColumn']['column'], "=", $label_value)
-                ->limit($limit)
-                ->sum($col)
-              ;
-            }
+        # Data processing
+        foreach ($dataset['dataColumns'] as $col_def) {
+          $col = key($col_def);
 
-            $dataset['data'] = $data;
-          break;
+          $dataQuery = $this->adios->getModel($dataset['model']);
 
-          # Target column in found rows is imported as data
-          default:
-            $dataset['data'] = $dataset['data']
-              ->orderBy('id', 'desc')
-              ->limit($limit)
-            ;
+          if ($dataset['where'] != '') {
+            $dataQuery = $dataQuery->where(...array_merge_recursive($dataset['where']));
+          }
 
-            $dataset['data'] = array_merge_recursive(...$dataset['data']
-              ->select($col)
+          switch ($dataset['function']) {
+            # Counts all found rows, useful if there are more rows than labels
+            case 'count':
+              $data = [];
+
+              foreach ($this->params['labelModel'] as $label_value) {
+                $data[] = $dataQuery
+                  ->where($dataset['labelColumn']['column'], "=", $label_value)
+                  ->limit($limit)
+                  ->count();
+              }
+
+              $dataset['data'] = $data;
+              break;
+
+            # Sums all found rows, useful if there are more rows than labels
+            case 'sum':
+              $data = [];
+              foreach ($this->params['labelModel'] as $label_value) {
+                $data[] = $dataQuery
+                  ->where($dataset['labelColumn']['column'], "=", $label_value)
+                  ->limit($limit)
+                  ->sum($col);
+              }
+
+              $dataset['data'] = $data;
+              break;
+
+            # Target column in found rows is imported as data
+            default:
+              $dataQuery = $dataQuery
+                ->orderBy('id', 'desc')
+                ->limit($limit);
+
+              $dataset['data'] = array_merge_recursive(...$dataQuery
+                ->select($col)
+                ->get()
+                ->toArray())[$col] ?? [];
+          }
+        }
+
+        # Lastly, replace the label if it is a foreign ID
+        if ($dataset['labelColumn']['lookup']) {
+          foreach ($this->params['labelModel'] as &$label) {
+            $labelModel = $this->adios->getModel($dataset['labelColumn']['model']);
+
+            $labelData = $labelModel
+              ->select($dataset['labelColumn']['lookup'])
+              ->where('id', '=', $label)
               ->get()
-              ->toArray())[$col] ?? []
-            ;
-        }
-      }
+              ->toArray();
 
-      # Lastly, replace the label if it is a foreign ID
-      if ($dataset['labelColumn']['lookup']) {
-        foreach ($this->params['labelModel'] as &$label) {
-          $labelModel = $this->adios->getModel($dataset['labelColumn']['model']);
-
-          $labelData = $labelModel
-            ->select($dataset['labelColumn']['lookup'])
-            ->where('id', '=', $label)
-            ->get()
-            ->toArray()
-          ;
-
-          $label = $labelData[0][$dataset['labelColumn']['lookup']];
+            $label = $labelData[0][$dataset['labelColumn']['lookup']];
+          }
         }
       }
     }
@@ -134,7 +140,8 @@ class Chart extends \ADIOS\Core\View {
     $this->params['uid'] = $this->adios->uid;
   }
 
-  public function getTwigParams(): array {
+  public function getTwigParams(): array
+  {
     return $this->params;
   }
 }
