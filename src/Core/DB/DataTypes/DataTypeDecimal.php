3d25208456b5e3a69331e756b638fcf9f93f7bdc
Dusan <dusan.daniska@wai.sk>
2023-08-22 14:44:42 +0200

    indent 2 spaces

--- src/Core/DB/DataTypes/DataTypeDecimal.php
+++ src/Core/DB/DataTypes/DataTypeDecimal.php
@@ -15,81 +15,81 @@ namespace ADIOS\Core\DB\DataTypes;
  */
 class DataTypeDecimal extends \ADIOS\Core\DB\DataType
 {
-    public function get_sql_create_string($table_name, $col_name, $params = [])
-    {
-        $params['sql_definitions'] = '' != trim((string) $params['sql_definitions']) ? $params['sql_definitions'] : ' default null ';
-        $width = min(max((int) $params['byte_size'], 1), 65);
-        $decimals = min(min(max((int) $params['decimals'], 0), 30), $width);
+  public function get_sql_create_string($table_name, $col_name, $params = [])
+  {
+    $params['sql_definitions'] = '' != trim((string) $params['sql_definitions']) ? $params['sql_definitions'] : ' default null ';
+    $width = min(max((int) $params['byte_size'], 1), 65);
+    $decimals = min(min(max((int) $params['decimals'], 0), 30), $width);
 
-        $sqlDataType = ($params['sql_data_type'] ?? "decimal");
+    $sqlDataType = ($params['sql_data_type'] ?? "decimal");
 
-        if (!in_array($sqlDataType, ["double", "float", "decimal", "numeric"])) {
-            $sqlDataType = "decimal";
-        }
-
-        return "`{$col_name}` {$sqlDataType}($width, $decimals) {$params['sql_definitions']}";
+    if (!in_array($sqlDataType, ["double", "float", "decimal", "numeric"])) {
+      $sqlDataType = "decimal";
     }
 
-    public function get_sql_column_data_string($table_name, $col_name, $value, $params = [])
-    {
-        $params = _put_default_params_values($params, [
-            'null_value' => false,
-            'dumping_data' => false,
-            'escape_string' => $this->adios->getConfig('m_datapub/escape_string', true),
-        ]);
+    return "`{$col_name}` {$sqlDataType}($width, $decimals) {$params['sql_definitions']}";
+  }
 
-        if ($params['dumping_data'] && '' == $value) {
-            $value = '-1';
-        }
+  public function get_sql_column_data_string($table_name, $col_name, $value, $params = [])
+  {
+    $params = _put_default_params_values($params, [
+      'null_value' => false,
+      'dumping_data' => false,
+      'escape_string' => $this->adios->getConfig('m_datapub/escape_string', true),
+    ]);
 
-        $value = str_replace(',', '.', $value);
-
-        if ($params['null_value'] or '' == $value) {
-            return "$col_name=NULL";
-        } else {
-            if (is_numeric($value) && '' != $value) {
-                return "$col_name='" . ($params['escape_string'] ? $this->adios->db->escape($value) : $value) . "'";
-            } else {
-                return "$col_name=null";
-            }
-        }
+    if ($params['dumping_data'] && '' == $value) {
+      $value = '-1';
     }
 
-    public function get_html($value, $params = [])
-    {
-        $html = '';
+    $value = str_replace(',', '.', $value);
 
-        if (isset($params['col_definition']['decimals'])) {
-            $decimals = $params['col_definition']['decimals'];
-        } else {
-            $decimals = 2;
-        }
+    if ($params['null_value'] or '' == $value) {
+      return "$col_name=NULL";
+    } else {
+      if (is_numeric($value) && '' != $value) {
+        return "$col_name='" . ($params['escape_string'] ? $this->adios->db->escape($value) : $value) . "'";
+      } else {
+        return "$col_name=null";
+      }
+    }
+  }
 
-        $value_number = number_format($value + 0, $decimals, ',', ' '); // str_replace(".", ",", strip_tags($value));
+  public function get_html($value, $params = [])
+  {
+    $html = '';
 
-        if ('' == $params['col_definition']['format']) {
-            $html = $value_number;
-        } else {
-            $html = str_replace('{%VALUE%}', $value_number, $params['col_definition']['format']);
-        }
-
-        if ($params['col_definition']['unit'] != "") $html .= " {$params['col_definition']['unit']}";
-
-        return $html;
+    if (isset($params['col_definition']['decimals'])) {
+      $decimals = $params['col_definition']['decimals'];
+    } else {
+      $decimals = 2;
     }
 
-    public function get_csv($value, $params = [])
-    {
-        return str_replace('.', ',', strip_tags($value + 0));
+    $value_number = number_format($value + 0, $decimals, ',', ' '); // str_replace(".", ",", strip_tags($value));
+
+    if ('' == $params['col_definition']['format']) {
+      $html = $value_number;
+    } else {
+      $html = str_replace('{%VALUE%}', $value_number, $params['col_definition']['format']);
     }
 
-    public function fromString(?string $value)
-    {
-        return (float) $value;
-    }
+    if ($params['col_definition']['unit'] != "") $html .= " {$params['col_definition']['unit']}";
 
-    public function validate($value): bool
-    {
-        return empty($value) || is_float($value);
-    }
+    return $html;
+  }
+
+  public function get_csv($value, $params = [])
+  {
+    return str_replace('.', ',', strip_tags($value + 0));
+  }
+
+  public function fromString(?string $value)
+  {
+    return (float) $value;
+  }
+
+  public function validate($value): bool
+  {
+    return empty($value) || is_float($value);
+  }
 }
