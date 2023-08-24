9235695df35fca00ebfdcd040a0124395cdb96c3
Peter Alaxin <peter.alaxin@wai.sk>
2023-08-24 09:59:52 +0200

    Úprava dátového typ TEXT

--- src/Core/DB/DataTypes/DataTypeText.php
+++ src/Core/DB/DataTypes/DataTypeText.php
@@ -22,11 +22,19 @@ namespace ADIOS\Core\DB\DataTypes;
  */
 class DataTypeText extends \ADIOS\Core\DB\DataType
 {
-  public function get_sql_create_string($table_name, $col_name, $params = []) {
-    return "`{$col_name}` text ".($params['sql_definitions'] ?? "");
+  public function get_sql_create_string($table_name, $col_name, $params = [])
+  {
+    $sqlDataType = ($params['sql_data_type'] ?? "text");
+
+    if (!in_array($sqlDataType, ["tinytext", "text", "mediumtext", "longtext"])) {
+      $sqlDataType = "text";
+    }
+
+    return "`{$col_name}` {$sqlDataType} " . ($params['sql_definitions'] ?? "");
   }
 
-  public function get_sql_column_data_string($table_name, $col_name, $value, $params = []) {
+  public function get_sql_column_data_string($table_name, $col_name, $value, $params = [])
+  {
     $params = _put_default_params_values($params, [
       'null_value' => false,
       'dumping_data' => false,
@@ -36,17 +44,18 @@ class DataTypeText extends \ADIOS\Core\DB\DataType
     if ($params['null_value']) {
       $sql = "`{$col_name}` = NULL";
     } else {
-      $sql = "`{$col_name}` = '".($params['escape_string'] ? $this->adios->db->escape($value) : $value)."'";
+      $sql = "`{$col_name}` = '" . ($params['escape_string'] ? $this->adios->db->escape($value) : $value) . "'";
     }
 
     return $sql;
   }
 
-  public function get_html($value, $params = []) {
+  public function get_html($value, $params = [])
+  {
     $value = 'yes' == $params['col_definition']['wa_list_no_html_convert'] ? $value : strip_tags($value);
     $html = mb_substr($value, 0, ($params['col_definition']['wa_list_char_length'] ? $params['col_definition']['wa_list_char_length'] : 80), 'utf-8');
     if (strlen($html) < strlen($value)) {
-        $html .= '...';
+      $html .= '...';
     }
     $html = ($html);
 
