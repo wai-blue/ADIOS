dd074ac9ecf59b4fa913447ef688b8f8519d0e14
Dusan <dusan.daniska@wai.sk>
2024-09-06 14:49:50 +0200

    varchar predefined values

--- src/Components/Inputs/Varchar.tsx
+++ src/Components/Inputs/Varchar.tsx
@@ -2,15 +2,32 @@ import React, { Component } from 'react'
 import { Input, InputProps, InputState } from '../Input'
 import * as uuid from 'uuid';
 
-export default class Varchar extends Input<InputProps, InputState> {
+interface VarcharInputState extends InputState {
+  showPredefinedValues: boolean,
+}
+
+export default class Varchar extends Input<InputProps, VarcharInputState> {
   static defaultProps = {
     inputClassName: 'varchar',
     id: uuid.v4(),
     type: 'text',
   }
 
+  constructor(props: InputProps) {
+    super(props);
+
+    this.state = this.getStateFromProps(props);
+  }
+
+  getStateFromProps(props: InputProps) {
+    return {
+      ...this.state, // Parent state
+      showPredefinedValues: false,
+    };
+  }
+
   renderInputElement() {
-    return (
+    return <>
       <input
         type='text'
         value={this.state.value}
@@ -23,6 +40,24 @@ export default class Varchar extends Input<InputProps, InputState> {
         }
         disabled={this.state.readonly}
       />
-    );
+      {this.props.params?.predefinedValues ?
+        this.state.showPredefinedValues ?
+          <div className="mt-1">
+            <select
+              onChange={(e) => {
+                this.setState({value: e.currentTarget.value});
+              }}
+            >
+              {this.props.params?.predefinedValues.map((item: string) => {
+                return <option value={item}>{item}</option>
+              })}
+            </select>
+          </div>
+        :
+          <button className="mt-1 btn btn-small btn-transparent" onClick={() => { this.setState({showPredefinedValues: true}); }}>
+            <span className="text text-xs">Choose...</span>
+          </button>
+      : null}
+    </>;
   }
 }
