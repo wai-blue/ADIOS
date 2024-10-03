f3bb64fdefc7ed31383975be915bc68207a7871b
Dusan <dusan.daniska@wai.sk>
2024-10-03 12:48:59 +0200

    enumvalues btnstyle disabled

--- src/Components/Inputs/EnumValues.tsx
+++ src/Components/Inputs/EnumValues.tsx
@@ -60,7 +60,7 @@ export default class EnumValues extends Input<EnumValuesInputProps, InputState> 
         const enumCssClass = this.props.enumCssClasses ? (this.props.enumCssClasses[key] ?? '') : '';
         return <>
           <button
-            className={"btn " + (this.state.value == key ? (this.state.readonly ? "btn-disabled" : "btn-primary") : "btn-light") + " " + enumCssClass}
+            className={"btn " + (this.state.readonly ? "btn-disabled" : "") + " " + (this.state.value == key ? "btn-primary" : "btn-light") + " " + enumCssClass}
             onClick={() => { if (!this.state.readonly) this.onChange((this.state.value == key ? null : key)); }}
           >
             <span className="text">{enumValue}</span>
